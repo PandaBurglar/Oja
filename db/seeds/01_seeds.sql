7228463118ecb52d51578b8c7e0f1943bf5a357b
PandaBurglar <zane.bokhari@gmail.com>
2022-02-11 14:12:39 -0500

    create fake data to load database tables

---
+++ db/seeds/01_seeds.sql
@@ -0,0 +1,13 @@
+INSERT INTO users (username, email, password, phone_number, admin)
+VALUES ('Georgina Bada ', 'mkkay@gmail.com', 'password', '123456789', true),
+('Hashim Ahmed', 'hsahmed@gmail.com', 'password', '124576789', false ),
+('Martin Pham', 'mpham@hotmail.com', 'password', '2893536768', false ),
+('Zane Bokhari', 'zbokhari@gmail.com', 'password', '6475896758', true ),
+('Anon myous', 'anonmyous@icloud.com', 'password', '613222457', false ),
+('Cassie Ken', 'ccsken@gmail.com', 'password', '8987786565', false ),
+('Homo Sapien', 'homosapien@gmail.com', 'password', '7768987007', false ),
+('Lilly Singh', 'lillypad@singh.com', 'password', '3432226457', false ),
+('Rofiat Hubalot', 'githubiscool@myspace.com', 'password', '999767888', true ),
+('Apple McPhee', 'mcpheeapple@gmail.com', 'password', '4162893536', false),
+('Hashim Pham', 'hashimpham@gmail.com', 'password', '4162893536', false );
+
